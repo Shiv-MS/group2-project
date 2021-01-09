dcfd75033ec11d4b52685a954a3ad00c2cfc0bbb
Shiv <shewashivam@gmail.com>
2021-01-08 20:50:34 -0500

    heroku

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -3,6 +3,7 @@ const { Project } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
+    //validation
     const newProject = await Project.create({
       ...req.body,
       user_id: req.session.user_id,
@@ -11,6 +12,26 @@ router.post('/', async (req, res) => {
     res.status(200).json(newProject);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const projectData = await Project.update({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
