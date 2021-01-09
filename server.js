a3f055085e4935c8e3a4d0614790c581151b3be3
Shiv <shewashivam@gmail.com>
2021-01-08 23:58:26 -0500

    heroku6

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
+require("dotenv").config();
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
